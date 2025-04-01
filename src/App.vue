aadf666ad92e909e28278aa85bd9ed8fa9c3751e
8848TI <3022926092@qq.com>
2025-04-01 09:40:18 +0800

    1.初始化项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <el-button>aaa</el-button>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
